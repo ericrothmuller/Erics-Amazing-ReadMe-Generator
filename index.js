7d19a30a05875a3c1d914ecdddeafce7aac356e9
ericrothmuller <eric@skymarketingco.com>
2022-07-17 17:36:41 -0700

    Added form fields with checkbox of licenses included

--- index.js
+++ index.js
@@ -4,13 +4,39 @@
  .prompt([
     {
         type: "input",
-        message: "What is your name?",
-        name: "username",
+        message: "What is the title of your project?",
+        name: "title",
     },
     {
-        type: "password",
-        message: "What is your password?",
-        name: "password",
+        type: "input",
+        message: "What is your project's description?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "How would someone install their project?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "How would someone use your project",
+        name: "use",
+    },
+    {
+        type: "checkbox",
+        message: "What type of license does your project have?",
+        choices: ["GNU", "MIT", "BSD 2", "BSD 3", "Boost", "Creative Commons Zero", "Eclipse", "GNU Affero", "GNU General Public", "GNU Lesser", "Mozilla", "The Unlicense"],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "Who contributed to your project?",
+        name: "contributions",
+    },
+    {
+        type: "input",
+        message: "What type of license does your project have?",
+        name: "license",
     },
  ])
  .then ((response) => {
