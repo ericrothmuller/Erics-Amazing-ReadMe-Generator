05270c16d6dd38d99f40fb8a258b452896a7bb60
ericrothmuller <eric@skymarketingco.com>
2022-07-17 22:20:37 -0700

    Added all license badges to prepend if a user selects that license and generated an example README.md

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-# sdf
+# Eric's Amazing ReadMe Generator
 
-sdf
+This project, which I named Eric's Amazing ReadMe Generator, is part of the U.C. Berkeley Extension Coding Boot Camp. My name is Eric Rothmuller and I am a student of the Boot Camp. In this project I was tasked with the creating of a ReadMe Generator through Node prompts.
 
 
 ## Getting Started and Installing Guide
 
-sdf
+Please go to GitHub.com/ericrothmuller/erics-amazing-readme-generator and download the zip file to your machine.
 
 ## Table Of Contents
 
@@ -19,34 +19,34 @@ sdf
 
 ## Usage
 
-sfd
+Use it by running node index.js within the terminal (must have node installed) and then answer the prompts to generate a ReadMe file.
 
 
 ## Credits
 
-sdf
+Eric Rothmuller
 
 
 ## Tests
 
-sdf
+The test example is in this repository and it's called index.js. Run this in your terminal to generate the prompts that'll allow you to generate a ReadMe file.
 
 
 ## Questions
 
 ### Why was this built?
 
-sfd
+I built this for the U.C. Berkeley Extension program in order to learn more about node.
 
 ###  What problem does it solve?
 
-sfd
+It helps a user generate a quality ReadMe.
 
 ### What is your contact information?
 
-[GitHub Profile](https://github.com/sfd)
+[GitHub Profile](https://github.com/ericrothmuller)
 
-If you have additional questions, please email me at sf.
+If you have additional questions, please email me at eric@skymarketingco.com.
 
 ## License
 
--- index.js
+++ index.js
@@ -121,8 +121,86 @@ ${response.license}`;
 
     
     fs.writeFile("./README.md", content, err => {
+        console.log(response);
+        if (response.license == "GNU") {
+            prepend("README.md", "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
         if (response.license == "MIT") {
             prepend("README.md", "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "BSD 2") {
+            prepend("README.md", "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "BSD 3") {
+            prepend("README.md", "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "Boost") {
+            prepend("README.md", "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "Creative Commons Zero") {
+            prepend("README.md", "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "Eclipse") {
+            prepend("README.md", "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "GNU Affero") {
+            prepend("README.md", "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "GNU General Public") {
+            prepend("README.md", "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "GNU Lesser") {
+            prepend("README.md", "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "Mozilla") {
+            prepend("README.md", "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)", err => {
+                if (err) {
+                    console.error(err);
+                }
+            })
+        }
+        if (response.license == "The Unlicense") {
+            prepend("README.md", "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)", err => {
                 if (err) {
                     console.error(err);
                 }
